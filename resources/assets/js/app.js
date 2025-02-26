799734012324e648126321453a62dbfd6e65282e
Bertalan Fodor <fodber@organum.hu>
2025-02-26 08:26:51 +0000

    Add more interstitials to searches.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,3 +2,15 @@ import './pages/home.js';
 import './pdfDialog.js';
 import('./quickSearch.js');
 import('./textDisplay.js');
+
+$('#searchForm').on('submit', function(event) {
+    event.preventDefault(); 
+    $('.interstitial').show();
+    event.target.submit(); // Submit the form after showing the interstitial
+});
+
+$('#semanticSearchForm').on('submit', function(event) {
+    event.preventDefault(); 
+    $('.interstitial').show();
+    event.target.submit(); // Submit the form after showing the interstitial
+});
